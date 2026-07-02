9ebb18f860280931159182f84c5698b3ca8685e7
Uvaish Khan <uvaishkhanwork@gmail.com>
2026-07-02 21:42:23 +0530

    imp auth

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -44,8 +44,7 @@ export default function Login() {
     // TODO: wire to real auth API. For now, mark the session as signed in and
     // return the user wherever they came from (e.g. the course they were buying).
     setLoggedIn();
-    const redirect = new URLSearchParams(window.location.search).get("redirect");
-    router.push(redirect || "/");
+    router.push(getPostAuthRedirectTarget());
   };
 
   return (
