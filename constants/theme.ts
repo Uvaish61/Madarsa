8170ce2eb6d590a9e51c51fd0fa75128aacf8428
Uvaish Khan <uvaishkhanwork@gmail.com>
2026-07-03 23:29:14 +0530

    contants

---
+++ constants/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  primary: "#1a4731",
+  accent: "#20c997",
+  accentBg: "#e8f8f2",
+  border: "#e8ebe8",
+  textPrimary: "#111",
+  textMuted: "#888",
+  cardBg: "#fff",
+  pageBg: "#f3f4f2",
+};
